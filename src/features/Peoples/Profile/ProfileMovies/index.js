625a14cc06da647d7a87a45d306af3a8736c97f4
Katarzyna Maculewicz <qla.katarzyna@gmail.com>
2026-03-02 21:31:01 +0000

    feat(moviePage, profile): add counts, hide empty sections, and format code

--- src/features/Peoples/Profile/ProfileMovies/index.js
+++ src/features/Peoples/Profile/ProfileMovies/index.js
@@ -1,110 +1,126 @@
 import {
-    Wrapper,
-    PersonSection,
-    Header,
-    TileSection,
-    Tile,
-    TileImage,
-    MovieTile,
-    Title,
-    Year,
-    Tags,
-    Tag, 
-    RatingContent,
-    Rating,
-    Star,
-    Camera
+  Wrapper,
+  PersonSection,
+  Header,
+  TileSection,
+  Tile,
+  TileImage,
+  MovieTile,
+  Title,
+  Year,
+  Tags,
+  Tag,
+  RatingContent,
+  Rating,
+  Star,
+  Camera,
 } from "./styled";
 
 export const ProfileMovies = ({ credits, genresMap }) => (
-    <Wrapper>
-<PersonSection>
-                         <Header>Movies - cast ({credits?.cast?.length || 0})</Header>
-                        <TileSection >
-                            {credits?.cast?.map((movie) => (
-                                <Tile key={movie.id} to={`/movies/${movie.id}`}>
-                            <TileImage>
-                                {movie.poster_path ? (
-                                    <img 
-                                    src={`https://image.tmdb.org/t/p/w185${movie.poster_path}`}
-                                    alt={movie.title}
-                                    />
-                                ) : (
-                                    <div><Camera /></div>
-                                )}
-                            </TileImage>
-                                <MovieTile>
-                                    <Title>{movie.title}</Title>
-                                    <Year>{movie.release_date ? 
-                                    new Date(movie.release_date).getFullYear() : "Unknown year"}</Year>
-                                    <Tags>
-                                        {movie.genre_ids?.map((id) => (
-                                            <Tag key={id}>{genresMap[id] || "Unknown"}</Tag>
-                                        ))}
-                                    </Tags>   
-                                
-                                    <RatingContent>
-                                        <Rating>
-                                            <Star /> {movie.vote_average === 0 ? ""
-                                             : `${movie.vote_average.toLocaleString("pl-PL", {
-                                                  minimumFractionDigits: 1,
-                                                  maximumFractionDigits: 1,
-                                             })}`}
-                                        </Rating>
-                                            {movie.vote_count === 0
-                                            ? "No votes yet"
-                                            : `${movie.vote_count} ${
-                                            movie.vote_count === 1 ? "vote" : "votes"
-                                            }`}
-                                    </RatingContent>   
-                                  </MovieTile>                      
-                            </Tile> 
-                            ))}                           
-                        </TileSection>
-                    </PersonSection>
-                    <PersonSection>
-                         <Header>Movies - crew ({credits?.crew?.length || 0})</Header>
-                         <TileSection >
-                            {credits?.crew?.map((movie) => (
-                                <Tile key={movie.id} to={`/movies/${movie.id}`}>
-                            <TileImage>
-                                {movie.poster_path ? (
-                                    <img 
-                                    src={`https://image.tmdb.org/t/p/w185${movie.poster_path}`}
-                                    alt={movie.title}
-                                    />
-                                ) : (
-                                  <div> <Camera /> </div> 
-                                )}
-                            </TileImage>
-                                <MovieTile>
-                                    <Title>{movie.title}</Title>
-                                    <Year>{movie.release_date ? 
-                                    new Date(movie.release_date).getFullYear() : "Unknown year"}</Year>
-                                    <Tags>
-                                        {movie.genre_ids?.map((id) => (
-                                            <Tag key={id}>
-                                                {genresMap[id] || "Unknown"}
-                                            </Tag>
-                                        ))}
-                                    </Tags>
-                                
-                                    <RatingContent>
-                                        <Rating>
-                                            <Star />{movie.vote_average.toFixed(1)}
-                                        </Rating>
-                                            {movie.vote_count === 0
-                                            ? "No votes yet"
-                                            : `${movie.vote_count} ${
-                                            movie.vote_count === 1 ? "vote" : "votes"
-                                            }`}
-                                    </RatingContent>   
-                                  </MovieTile>                      
-                            </Tile> 
-                            ))}                           
-                        </TileSection>
-                    </PersonSection>
-</Wrapper>
+  <Wrapper>
+    <PersonSection>
+      {credits?.cast?.length > 0 && (
+        <>
+          <Header>Movies - cast ({credits.cast.length})</Header>
+          <TileSection>
+            {credits.cast.map((movie) => (
+              <Tile key={movie.id} to={`/movies/${movie.id}`}>
+                <TileImage>
+                  {movie.poster_path ? (
+                    <img
+                      src={`https://image.tmdb.org/t/p/w185${movie.poster_path}`}
+                      alt={movie.title}
+                    />
+                  ) : (
+                    <div>
+                      <Camera />
+                    </div>
+                  )}
+                </TileImage>
+                <MovieTile>
+                  <Title>{movie.title}</Title>
+                  <Year>
+                    {movie.release_date
+                      ? new Date(movie.release_date).getFullYear()
+                      : "Unknown year"}
+                  </Year>
+                  <Tags>
+                    {movie.genre_ids?.map((id) => (
+                      <Tag key={id}>{genresMap[id] || "Unknown"}</Tag>
+                    ))}
+                  </Tags>
+
+                  <RatingContent>
+                    <Rating>
+                      <Star />{" "}
+                      {movie.vote_average === 0
+                        ? ""
+                        : movie.vote_average.toLocaleString("pl-PL", {
+                            minimumFractionDigits: 1,
+                            maximumFractionDigits: 1,
+                          })}
+                    </Rating>
+                    {movie.vote_count === 0
+                      ? "No votes yet"
+                      : `${movie.vote_count} ${movie.vote_count === 1 ? "vote" : "votes"}`}
+                  </RatingContent>
+                </MovieTile>
+              </Tile>
+            ))}
+          </TileSection>
+        </>
+      )}
+
+      {credits?.crew?.length > 0 && (
+        <>
+          <Header>Movies - crew ({credits.crew.length})</Header>
+          <TileSection>
+            {credits.crew.map((movie) => (
+              <Tile key={movie.id} to={`/movies/${movie.id}`}>
+                <TileImage>
+                  {movie.poster_path ? (
+                    <img
+                      src={`https://image.tmdb.org/t/p/w185${movie.poster_path}`}
+                      alt={movie.title}
+                    />
+                  ) : (
+                    <div>
+                      <Camera />
+                    </div>
+                  )}
+                </TileImage>
+                <MovieTile>
+                  <Title>{movie.title}</Title>
+                  <Year>
+                    {movie.release_date
+                      ? new Date(movie.release_date).getFullYear()
+                      : "Unknown year"}
+                  </Year>
+                  <Tags>
+                    {movie.genre_ids?.map((id) => (
+                      <Tag key={id}>{genresMap[id] || "Unknown"}</Tag>
+                    ))}
+                  </Tags>
+
+                  <RatingContent>
+                    <Rating>
+                      <Star />{" "}
+                      {movie.vote_average === 0
+                        ? ""
+                        : movie.vote_average.toFixed(1)}
+                    </Rating>
+                    {movie.vote_count === 0
+                      ? "No votes yet"
+                      : `${movie.vote_count} ${movie.vote_count === 1 ? "vote" : "votes"}`}
+                  </RatingContent>
+                </MovieTile>
+              </Tile>
+            ))}
+          </TileSection>
+        </>
+      )}
+    </PersonSection>
+  </Wrapper>
 );
 
 export default ProfileMovies;
